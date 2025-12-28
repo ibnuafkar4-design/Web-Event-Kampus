df22649653fa70d90f56b2ecb6e736424b3144b6
anisyavieen <anisyamiftahuljannah17@gmail.com>
2025-12-28 13:36:57 +0700

    ubah logo login

--- public/login.php
+++ public/login.php
@@ -94,7 +94,7 @@ body {
         </h3>
         <form id="loginForm" method="POST" action="login.php">
             <h5 class="text-center mb-3">
-                <i class="fa-solid fa-right-to-bracket me-2"></i>Login Admin
+                <i class="fa-solid fa-key me-2"></i>Login Admin
             </h5>
             <input type="text" name="username" class="form-control mb-3" id="username" placeholder="Username atau Email"
                 required />
